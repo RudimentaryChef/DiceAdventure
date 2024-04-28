7c84a99be0a03d62385f669c0af6c6e3663311b8
Rudimentary_Chef <akrish6@gatech.edu>
2024-04-28 00:40:21 -0400

    fixes forksafetychecker

--- checkForkSafety.py
+++ checkForkSafety.py
@@ -1,8 +1,4 @@
 import os
 
 objc_disable_initialize_fork_safety = os.getenv("OBJC_DISABLE_INITIALIZE_FORK_SAFETY")
-
-if objc_disable_initialize_fork_safety == "yes":
-    print("yes")
-else:
-    print("no")
+print(objc_disable_initialize_fork_safety)
